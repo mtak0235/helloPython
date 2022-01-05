9f5534b7c7fe787d533b4f92aa3753577bea3c20
mtak0235 <jesustark0235@gmail.com>
2022-01-05 21:06:01 +0900

    #4 --count가 안먹음

--- step3/mtak/multiinput.py
+++ step3/mtak/multiinput.py
@@ -1,4 +1,8 @@
 count = int(input())
+result = []
 while count > 0:
-    --count
-    [a,b] = input().split() 
+    count -= 1
+    [a,b] = input().split()
+    result.append(int(a) + int(b))
+for p in result:
+    print(p)
