bde0547fff4516f291579e1e5e39a02838ed097e
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-01-18 01:59:57 +0900

    Revert "1/18 step2 fin"
    This reverts commit adae0f4e2a49c6565ef01a142520f4795f67ee4f.

--- step2/1330.py
+++
@@ -1,9 +0,0 @@
-A, B = map(int, input().split())
-
-print("<") if A < B else print(">") if A > B else print("==") 
-# if A < B:
-#     print("<")
-# elif A > B:
-#     print(">")
-# else:
-#     print("==")
