2ec4455fa58a7d0ba744d4e8229ba07fd3b098c7
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-01-18 00:17:44 +0900

    recovered 10430.py

--- step1/10430.py
+++ step1/10430.py
@@ -1,14 +1,5 @@
 from __future__ import print_function
 import sys
 
-A = int(sys.stdin.readline())
-B = int(sys.stdin.readline())
-
-base = 10
-tmp = B % base
-while tmp != B:
-    print(A * tmp)
-    base *= 10
-    tmp = B % base
-print(A * tmp)
-
+A, B, C = map(int, sys.stdin.readline().split())
+print((A+B)%C, ((A%C)+(B%C))%C, (A*B)%C, ((A%C)*(B%C))%C, sep='\n')
