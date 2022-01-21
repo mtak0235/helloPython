7efe0fb3771e27108f8fb73b048ed845cba5b727
Daebeom Cho <dcho@c1r6s5.42seoul.kr>
2022-01-21 14:11:24 +0900

    [10952] step4 a+b-5

--- step4/dcho/a+b-5.py
+++ step4/dcho/a+b-5.py
@@ -4,6 +4,6 @@ import sys
 
 while True:
 	a, b = map(int, sys.stdin.readline().split())
-	if ((a < 0 and a > 10) and (b < 0 and b > 10) or (a == 0 or b == 0)):
+	if (a < 0 or a > 10 or b < 0 or b > 10 or (a == 0 or b == 0)):
 		break
 	print(a + b)
