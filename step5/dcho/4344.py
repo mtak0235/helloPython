0ad7e7225faf9104300bf02c2bb4b09ef2745f34
DBrider3 <tmdgns743@gmail.com>
2022-01-27 16:35:22 +0900

    [4344] step5, above average

--- step5/dcho/4344.py
+++ step5/dcho/4344.py
@@ -5,12 +5,18 @@ import sys
 n = int(sys.stdin.readline())
 doublelist = []
 averagelist = [0 for i in range(n)]
+studentRate = [0 for i in range(n)]
 
 for i in range(n):
-	doublelist.append(list(map(float, sys.stdin.readline().split())))
-	# averagelist[i] += float(x) for x in range(len(doublelist[i]) - 1)
-	print(len(doublelist[i]) - 1)
-	for j in range(len(doublelist[i]) - 1):
-		doublelist[j + 1]
 
-print(averagelist)
+	doublelist.append(list(map(float, sys.stdin.readline().split())))
+	for j in range(len(doublelist[i]) - 1):
+		averagelist[i] += doublelist[i][j + 1]
+	averagelist[i] = averagelist[i] / doublelist[i][0]
+	for j in range(len(doublelist[i]) - 1):
+		if averagelist[i] < doublelist[i][j + 1]:
+			studentRate[i] += 1
+	studentRate[i] = studentRate[i] / doublelist[i][0] * 100
+for i in studentRate:
+	print('{i:.03f}%'.format(i = i))
+
