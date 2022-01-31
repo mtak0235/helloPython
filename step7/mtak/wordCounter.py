81cd9fa1e67df57265f432582765815d5b9fbece
Min kyeong Tak <mtak0235@gmail.com>
2022-01-31 15:36:13 +0900

    [1316]step7,그룹 단어 체크

--- step7/mtak/wordCounter.py
+++ step7/mtak/wordCounter.py
@@ -1,5 +1,21 @@
 import sys
-_res = 0
-for i in range(sys.stdin.readline()):
-    for i in range(ord('a'), ord('z') + 1):
-        
+import copy
+_check = 0
+_cnt = int(sys.stdin.readline())
+_res = _cnt
+_input = list()
+_set = list()
+for i in range(_cnt):
+    _input.append(sys.stdin.readline().strip())
+    _set.append(set(_input[i]))
+for i in range(_cnt):
+    _check = 0
+    for c in _set[i]:
+        tmp = _set[i] - set(c)
+        for t in tmp:
+            if t in _input[i][_input[i].index(c):_input[i].rindex(c)]:
+                _res -= 1
+                _check = 1
+                break
+        if _check == 1 : break
+print(_res)
