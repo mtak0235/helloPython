44b7b68cf70aaf41424e2b91be9fc13e2b840d1b
Sara-Jo <sr930927@gmail.com>
2022-02-14 11:27:38 +0900

    [2798] step11, 블랙잭

---
+++ step11/sjo/2798블랙잭.py
@@ -0,0 +1,26 @@
+# 1 ---------------------------------------------
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+result = 0 
+
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            if arr[i] + arr[j] + arr[k] <= m:
+                result = max(result, arr[i] + arr[j] + arr[k])
+
+print(result)
+
+# 2 ---------------------------------------------
+# from itertools import combinations
+
+# n, m = map(int, input().split())
+# arr = list(map(int, input().split()))
+# result = 0
+
+# for cards in combinations(arr, 3):
+#     temp_sum = sum(cards)
+#     if result < temp_sum <= m:
+#         result = temp_sum
+
+# print(result)
