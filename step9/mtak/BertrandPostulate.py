db27a05ea7f7ca5c5b60dc27dc2c2fecef06d249
mtak0235 <jesustark0235@gmail.com>
2022-02-18 23:56:18 +0900

    [4948] step9, 베르트랑 공준

---
+++ step9/mtak/BertrandPostulate.py
@@ -0,0 +1,20 @@
+import sys
+import math
+def isPrimary(n):
+    for i in range(2, math.floor(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+_input = -1
+while _input != 0:
+    _input = int(sys.stdin.readline())
+    if _input == 1:
+        print(1)
+        continue
+    cnt = 0
+    for i in range(_input, 2 * _input + 1):
+        if isPrimary(i):
+            cnt += 1
+    print(cnt)
+
