f64a1f4c7f31296831a3f65f086bf3cd19a35c5e
arkingco <arkingco@gmail.com>
2022-02-23 23:47:57 +0900

    [1316] step7, 그룹 단어 체크

--- step7/kipark/1316.py
+++ step7/kipark/1316.py
@@ -1,12 +1,17 @@
 import sys
 
 N = int(input())
-arr = list()
+
 result = 0
 
 for i in range(N):
-	arr.append(input())
-
-for i in range(N):
-	for j in range(len(arr[i])):
-		
+	arr = input()
+	group_checker = 0
+	for i in range(0, len(arr)):
+		for j in range(i+1, len(arr)):
+			if(arr[i] == arr[j]) and (arr[i] != arr[j-1]):
+				group_checker = 1
+	if(group_checker == 0):
+		result += 1
+	
+print(result)
