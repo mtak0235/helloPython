1003fbe85336dbac3f7d41dd1e0f0225343dfae6
DBrider3 <tmdgns743@gmail.com>
2022-03-09 23:17:13 +0900

    [1978] step9, 소수찾기

---
+++ step9/dcho/1978.py
@@ -0,0 +1,16 @@
+# 1978
+
+import sys
+
+n: int = int(sys.stdin.readline())
+numbers = map(int, input().split())
+sosu: int = 0
+for num in numbers:
+    error = 0
+    if num > 1 :
+        for i in range(2, num):
+            if num % i == 0:
+                error += 1
+        if error == 0:
+            sosu += 1
+print(sosu)
