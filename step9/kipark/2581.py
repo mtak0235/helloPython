d602579140889bedbcf396c884c13f6c297b05f6
arkingco <arkingco@gmail.com>
2022-03-14 23:41:37 +0900

    [2581] step9, 소수 kipark

--- step9/kipark/2581.py
+++ step9/kipark/2581.py
@@ -1,17 +1,25 @@
 #[2581] step9, 소수 kipark
 import sys
 
-era = [False, False] + [True] * 10001
+era = [False, False, True] + [True] * 10001
 
 for i in range(2, 10001):
     if era[i]:
         for j in range(2*i, 10001, i):
             era[j] = False
 
-n = int(input())
-ret = 0
-numbers = list(map(int, sys.stdin.readline().split()))
-for num in numbers:
-    if era[num] == True:
-        ret += 1
-print(ret)
+M = int(input())
+N = int(input())
+
+result = 0
+row_result = 10002
+for nums in range(M, N+1):
+    if era[nums] == True:
+        result += nums
+        row_result = nums if row_result > nums else row_result
+
+if result > 0:
+    print(result)
+    print(row_result)
+else:
+    print(-1)
