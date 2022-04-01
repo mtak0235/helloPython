86798f4a36104d3c05da80eacf3384dd0790d7ed
mtak <jesustark0235@gmail.com>
2022-04-01 12:28:45 +0900

    [9184] 동적 계획법1 , 신나는 함수 실행 좀더 최적화

--- StepByStep/동적계획법1/9184.py
+++ StepByStep/동적계획법1/9184.py
@@ -21,3 +21,33 @@ while True:
     if a==-1 and b==-1 and c==-1:
         break
     print(f'w({a}, {b}, {c}) = {w(a,b,c)}')
+
+
+import sys
+
+
+def w(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    if _set[a][b][c]:
+        return _set[a][b][c]
+    if a < b < c:
+        _set[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return _set[a][b][c]
+    _set[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    return _set[a][b][c]
+
+_set = [[[0 for one in range(21)] for two in range(21)] for three in range(21)]
+
+while True:
+    a, b, c = map(int, sys.stdin.readline().split())
+    if a == -1 and b == -1 and c == -1:
+        break
+    if a <= 0 or b <= 0 or c <= 0:
+        print(f'w({a}, {b}, {c}) = {1}')
+        continue
+    if a > 20 or b > 20 or c > 20:
+        print(f'w({a}, {b}, {c}) = {w(20, 20, 20)}')
+        continue
+    print(f'w({a}, {b}, {c}) = {w(a,b,c)}')
+ç
