fb51550f65d6d2f92fc5c0f0b47998aedc93ae19
sunghkim <az0856@naver.com>
2022-04-19 02:08:26 +0900

    [2559]구간합, 수열

---
+++ StepByStep/누적합/2559.py
@@ -0,0 +1,10 @@
+import sys
+N, K = map(int,sys.stdin.readline().split())
+num = list(map(int,sys.stdin.readline().split()))
+DP = [0] * N
+MAX = sum(num[:K])
+tmp = MAX
+for i in range(N - K):
+    tmp = tmp - num[i] + num[i + K]
+    MAX = max(MAX, tmp)
+print(MAX)
