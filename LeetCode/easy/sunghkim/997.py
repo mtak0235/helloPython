460f9ace8537485587c4ee41277b89bf14808979
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-06-07 11:44:36 +0900

    Delete unnecessary files

--- LeetCode/easy/sunghkim/997.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Solution(object):
-    def sortedSquares(self, nums):
-        lst = np.array(nums)
-        lst = lst * lst
-        result = lst.sort()
-        return (lst)
-        
