25ad230638b1b3870130d724dd48c88c09122952
42hm <came@kakao.com>
2022-06-07 10:30:36 +0900

    [10430] 입출력과사칙연산, 나머지

---
+++ StepByStep/입출력과사칙연산/hmoon/10430.py
@@ -0,0 +1,8 @@
+import sys
+
+if __name__ == '__main__':
+    a, b, c = map(int, input().split())
+    print((a + b) % c)
+    print(((a % c) + (b % c)) % c)
+    print((a * b) % c)
+    print(((a % c) * (b % c)) % c)
