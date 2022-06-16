2cb21c7111fe8e8d84a95de5b6d71a97248e0b00
zizudana <zizudana@naver.com>
2022-06-17 00:51:20 +0900

    [15652] 백트래킹, N과M(4)

---
+++ StepByStep/백트래킹/daeukim/15652.py
@@ -0,0 +1,16 @@
+# N과 M (4)
+
+n,m = map(int, input().split())
+ 
+s = []
+def dfs(start):
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(start, n+1):
+        s.append(i)
+        dfs(i)
+        s.pop()
+    
+dfs(1)
