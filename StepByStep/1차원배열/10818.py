ac58933489cec2b88e259edf08b2e32862a97265
42hm <came@kakao.com>
2022-06-17 01:40:22 +0900

    경로 수정 및 삭제

--- StepByStep/1차원배열/10818.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-N = int(sys.stdin.readline())
-solves = list(map(int, sys.stdin.readline().split()))
-
-print(min(solves),max(solves))
--- StepByStep/1차원배열/2562.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-solves = []
-
-for i in range(0, 9):
-    solves.append(int(sys.stdin.readline()))
-
-print(max(solves))
-print(int(solves.index(max(solves)) + 1))
