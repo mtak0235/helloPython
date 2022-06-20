6982a9442f2c2c72c1ab449d25bb6e5e70da09d7
42hm <came@kakao.com>
2022-06-20 19:16:23 +0900

    [4673] 함수, 셀프 넘버_2nd

--- StepByStep/함수/hmoon/4673.py
+++ StepByStep/함수/hmoon/4673.py
@@ -1,31 +1,23 @@
-def selfnumber(num, cnt):
+import sys
+
+def selfnumber(num, nlist):
     ret = num
     temp = num
-    num = list(map(int, str(num)))
-    for nums in num:
-        ret += nums
-    cnt += 1
-    if (ret > 10000 and cnt == 1):
-        return (-2)
-    if (ret > 10000):
-        return (-1)
-    if ret != temp:
-        nlist.append(ret)
-    return (selfnumber(ret, cnt))
+    num_list = list(map(int, str(num)))
+    for each_num in num_list:
+        ret += each_num
+    if (ret > 10000 or ret == temp):
+        return (1)
+    try:
+        nlist.remove(ret)
+    except:
+        return (1)
+    return (selfnumber(ret, nlist))
 
 if __name__ == "__main__":
-    nlist = []
-    n = 1
-    while (1):
-        cnt = 0
-        ret = selfnumber(n, cnt)
-        if (ret == -1):
-            n += 1
-        if (ret == -2):
-            break
-    nlist = set(nlist)
-    nlist = list(nlist)
-    nlist.sort()
-    for i in range(1, 9997):
-        if i not in nlist:
-            print(i)
+    nlist = list(range(1, 10001))
+    for num in range(1, 10001):
+        ret = selfnumber(num, nlist)
+        if (ret == 1):
+            continue
+    print(*nlist, sep='\n')
