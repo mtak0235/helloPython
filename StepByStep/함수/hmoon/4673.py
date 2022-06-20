c5a1448a06240f3559757a1a75268b1382374359
42hm <came@kakao.com>
2022-06-20 19:28:01 +0900

    [4673] 함수, 셀프 넘버_완성

--- StepByStep/함수/hmoon/4673.py
+++ StepByStep/함수/hmoon/4673.py
@@ -2,22 +2,19 @@ import sys
 
 def selfnumber(num, nlist):
     ret = num
-    temp = num
     num_list = list(map(int, str(num)))
     for each_num in num_list:
         ret += each_num
-    if (ret > 10000 or ret == temp):
-        return (1)
+    if (ret > 10000):
+        return
     try:
         nlist.remove(ret)
     except:
-        return (1)
+        return
     return (selfnumber(ret, nlist))
 
 if __name__ == "__main__":
     nlist = list(range(1, 10001))
     for num in range(1, 10001):
         ret = selfnumber(num, nlist)
-        if (ret == 1):
-            continue
     print(*nlist, sep='\n')
