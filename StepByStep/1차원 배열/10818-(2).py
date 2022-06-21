f8644ec48a6edaeef56bd030629246362f3a3803
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-06-22 00:08:42 +0900

    Organize files

--- StepByStep/1차원 배열/10818-(2).py
+++
@@ -1,6 +0,0 @@
-import sys
-
-cnt = int(sys.stdin.readline().rstrip())
-numbers = list(map(int, sys.stdin.readline().rstrip().split()))
-
-print(min(numbers), max(numbers))
--- StepByStep/1차원 배열/2562.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-numbers = []
-for i in range(0, 9) :
-	numbers.append(int(sys.stdin.readline().rstrip()))
-print(max(numbers))
-print(numbers.index(max(numbers)) + 1)
