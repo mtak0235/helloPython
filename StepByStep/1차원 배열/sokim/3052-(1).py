c85c46d455bccd1528fb6e4c13ae9a985c6ca0d2
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-06-22 20:08:43 +0900

    [3052] 1차원 배열, 나머지(다른 풀이)

---
+++ StepByStep/1차원 배열/sokim/3052-(1).py
@@ -0,0 +1,15 @@
+import sys
+
+numbers = []
+remainders = []
+cnt = 10
+
+for i in range(10) :
+	numbers.append(int(sys.stdin.readline()))
+	remainders.append(numbers[i] % 42)
+for i in range(10) :
+	for j in range(i) :
+		if remainders[i] == remainders[j] :
+			cnt -= 1
+			break
+print(cnt)
