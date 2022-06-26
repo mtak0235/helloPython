809b1d3d5f9f00a79a9d5e2804eb951fba0c0888
mtak <jesustark0235@gmail.com>
2022-06-26 13:24:52 +0900

    [11279] 우선순위 큐, 최대 힙

---
+++ StepByStep/우선순위큐/mtak/11279.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+input = sys.stdin.readline
+n = int(input())
+a = [int(input()) for _ in range(n)]
+heap = []
+heapq.heapify(heap)
+for i in a:
+    if i == 0 and len(heap)==0:
+        print(0)
+    elif i == 0:
+        print(abs(heapq.heappop(heap)))
+    else:
+        heapq.heappush(heap, -1 * i)
+
+
