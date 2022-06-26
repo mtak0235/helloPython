16c49e3794d335f7348eedf57da4074aecfecf48
taeng42 <hyundong@innoaca.kr>
2022-06-26 18:02:47 +0900

    [1920] 이분탐색, 수 찾기(바이너리서치)

--- StepByStep/이분탐색/taeng/1920.py
+++ StepByStep/이분탐색/taeng/1920.py
@@ -5,9 +5,30 @@
 import sys
 
 
+def bin_search(target, numeric, l, r):
+    if l > r:
+        return 0
+
+    if l == r:
+        if numeric[l] == target:
+            return 1
+        else:
+            return 0
+
+    mid = (l + r) // 2
+    if numeric[mid] == target:
+        return 1
+    elif numeric[mid] > target:
+        return bin_search(target, numeric, l, mid)
+    else:
+        return bin_search(target, numeric, mid + 1, r)
+
+
 if __name__ == "__main__":
     _ = input()
-    numerics = set(sys.stdin.readline().strip().split())
+    numerics = sorted(map(int, sys.stdin.readline().strip().split()))
     _ = input()
-    targets = ["1" if n in numerics else "0" for n in sys.stdin.readline().strip().split()]
-    print("\n".join(targets))
+    targets = list(map(int, sys.stdin.readline().strip().split()))
+    for target in targets:
+        print(bin_search(target, numerics, 0, len(numerics) - 1))
+
