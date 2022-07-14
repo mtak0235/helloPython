a0a725639ea527b45ea9a860c26b8bb617e2d947
zizudana <zizudana@naver.com>
2022-07-14 20:26:09 +0900

    clear

--- LeetCode/easy/daeukim/49.py
+++
@@ -1,8 +0,0 @@
-# 그룹 애너그램
-
-def groupAagrams(self, strs:List[str]) -> List[List[str]]:
-	anagrams = collections.defaultdict(list)
-
-	for word in strs:
-		anagrams[''.join(sorted(word))].append(word)
-	return list(anagrams.values())
