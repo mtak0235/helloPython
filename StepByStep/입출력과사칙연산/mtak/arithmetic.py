d73a2d9449737468aaf8d387797cb92e19e44a2c
Min kyeong Tak <mtak0235@gmail.com>
2022-07-25 22:26:31 +0900

    [25083] 입출력과  사칙연산, 새싹

---
+++ StepByStep/입출력과사칙연산/mtak/arithmetic.py
@@ -0,0 +1,7 @@
+input = input()
+input_arr = input.split()
+print(int(input_arr[0]) + int(input_arr[1]))
+print(int(input_arr[0]) - int(input_arr[1]))
+print(int(input_arr[0]) * int(input_arr[1]))
+print(int(input_arr[0]) // int(input_arr[1]))
+print(int(input_arr[0]) % int(input_arr[1]))
